b1fe854a64de43ea5ad2fb38b95fd23a718cc28f
tshadrin <tshadrin@gmail.com>
2019-09-22 20:07:24 +0300

    initial commit

---
+++ chapter03/exercise31.cpp
@@ -0,0 +1,12 @@
+﻿// AcceleratedCpp.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello 897798CMake789787987897." << endl;
+	return 0;
+}
