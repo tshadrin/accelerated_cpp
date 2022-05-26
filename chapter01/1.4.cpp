2a404c7170698ebcf99a8288ede7bae048bf93ee
sham <reg@shadrin.me>
2022-05-26 12:44:33 +0300

    add standard c++17, fix chapter 1 listings

--- chapter01/1.4.cpp
+++ chapter01/1.4.cpp
@@ -3,6 +3,9 @@
 
 int main()
 {
+#ifdef __WIN32
+    system("chcp 65001");
+#endif
     { const std::string s{"Одна строка"};
       std::cout << s << std::endl; 
     { const std::string s{"Другая строка"};
--- chapter01/1.5.1.cpp
+++ chapter01/1.5.1.cpp
@@ -3,6 +3,9 @@
 
 int main()
 {
+#ifdef __WIN32
+    system("chcp 65001");
+#endif
     { std::string s{"Одна строка"};
     { std::string x{s + ", действительно"};
       std::cout << s << std::endl;
