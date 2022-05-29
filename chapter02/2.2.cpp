4f37f9d026b19b8d5b9ab11b711923035ac70cf1
sham <reg@shadrin.me>
2022-05-29 21:43:51 +0300

    fix chapter 2 exercise solutiions

--- chapter02/2.2.cpp
+++ chapter02/2.2.cpp
@@ -9,13 +9,13 @@ int main()
     string name;
     cin >> name;
     const string greeting = "Hello, " + name + "!";
-    const int l_pad = 6, r_pad = 9, t_pad = 7, b_pad = 5;
-    const int rows = t_pad + b_pad + 3;
-    const string::size_type cols = greeting.length() + l_pad + r_pad + 2;
+    const int w_pad = 6, h_pad = 7;
+    const int rows = h_pad * 2 + 3;
+    const string::size_type cols = greeting.length() + w_pad * 2 + 2;
     for(int r = 0; r != rows; ++r) {
         string::size_type c = 0;
         while(c != cols) {
-            if(r == t_pad + 1 && c == l_pad + 1) {
+            if(r == h_pad + 1 && c == w_pad + 1) {
                 cout << greeting;
                 c += greeting.length();
             } else {
